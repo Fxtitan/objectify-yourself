dc035ed634ae8598749748993b42ec4014344b83
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-03-26 09:13:29 -0400

    done

--- main.js
+++ main.js
@@ -3,46 +3,56 @@ const myCodeImmersivesObj = {
 };
 
 // Set the value of property `objectify` to be "yourself".
-
+objectify = "Sharod";
+console.log(objectify)
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
-
-
+myCodeImmersivesObj.isAnExcellentObject = true;
+console.log(myCodeImmersivesObj.isAnExcellentObject)
 // Add the property `term` and set it to 1.
-
+myCodeImmersivesObj.term = 1;
+console.log(myCodeImmersivesObj.term)
 
 // Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
-
+myCodeImmersivesObj.term ++;
+console.log(myCodeImmersivesObj.term)
 
 // Now add a new property called `currentScore` and set it to 3.
-
+myCodeImmersivesObj.currentScore = 3;
+console.log(myCodeImmersivesObj.currentScore)
 
 // Now add a new property called `cumulativeScore` and set it to 50.
-
+myCodeImmersivesObj.cumulativeScore = 50;
+console.log(myCodeImmersivesObj.cumulativeScore)
 
 // Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
-
+myCodeImmersivesObj.currentScore + myCodeImmersivesObj.cumulativeScore     //hmmmm??
+console.log(myCodeImmersivesObj.cumulativeScore)
 
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
+//??
 
 
 // Now make a new EMPTY object that represents yourself.
+const meImJustMe = {};
 
 
 // Add a `firstName` property and set it to a string.
-
+meImJustMe.firstName = "Sharod";
+console.log(meImJustMe.firstName);
 
 // Add a `lastName` property and set it to a string.
-
+meImJustMe.lastName = "Bailey";
 
 // Add a `fullName` property and set it to the name properties put together with a space string in the middle.
-
+meImJustMe.fullName = meImJustMe.firstName + [" "] + meImJustMe.lastName;
+console.log(meImJustMe.fullName)
 
 // Add an `age` property and set it to a number.
-
+meImJustMe.age = 29;
 
 // Add an address property and set it to an empty array.
-
+meImJustMe.address = [];
 
 // Now push three strings into that array.
 
@@ -58,6 +68,12 @@ const myCodeImmersivesObj = {
 // bracket notation as well. The important thing here is: this
 // should all be done WITHIN the brackets of the object, NOT
 // below the declaration of the object.
+const myLife = new Object();
+  myLife.situation = "Something Else";
+  myLife.value = "limitless?";
+  myLife.years = 29;
+  console.log(myLife)
+
 
 
 // As a further stretch goal, add a function that takes in a key name as a string and changes the corresponding value in the object you just made to be the string 'overwritten DYNAMICALLY'. Now call it with the name of one of your keys and see if it works! This will all take some research... Good luck!
